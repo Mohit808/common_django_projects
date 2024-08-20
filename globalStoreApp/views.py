a531dea49c8101b2b9bbfed063f7e5633430c58d
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2024-08-20 19:27:39 +0530

    Update views.py

--- globalStoreApp/views.py
+++ globalStoreApp/views.py
@@ -4,4 +4,15 @@ from django.http import JsonResponse
 # Create your views here.
 
 def index(request):
-    return  JsonResponse(data={"message": "Hello World"})
+    # return  JsonResponse(data={"message": "Hello World"})
+    return render(render, html)
+
+
+
+html= """ 
+<div class="demo">
+  <h1>Demo</h1>
+  <p>This is a demo html page. You can edit the HTML, SCSS and see the changes in real time.</p>
+  <p>Access to full source code editing and all functionalities is available in the paid version.</p>
+</div>
+"""
