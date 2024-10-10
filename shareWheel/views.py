b9f2b7eb167858b16d83127e7a1936032ba75763
AmitYadavTci <amit.yadav@tci.email>
2024-10-10 18:10:36 +0530

    .

--- shareWheel/views.py
+++ shareWheel/views.py
@@ -12,3 +12,10 @@ class WheelBooking(APIView):
         serializer = WheelBookingSerializer(query, many=True,context={'request': request})
         return customResponse(message= f'Fetch data successfully', status=200  ,data=serializer.data)
     
+    def post(self,request,pk=None):
+        serializer = WheelBookingSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            customResponse(message='created successfully', status=200)
+        else : 
+            customResponse(message='Failed to create', status=400, data=serializer.errors)
