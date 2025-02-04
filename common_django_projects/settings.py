8c3997b2fc768b51de2a06e1a885f926b404876c
Mohit808 <amityadav.ayx@gmail.com>
2025-02-05 02:19:06 +0530

    Add CORS support and update settings for shareWheel app

--- common_django_projects/settings.py
+++ common_django_projects/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'globalStoreApp',
     'waterDropApp',
-    'shareWheel'
+    'shareWheel',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,6 +120,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
