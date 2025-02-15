9cdcf88dce3a71bdad5ee5a195c0e6413012ac95
Mohit808 <amityadav.ayx@gmail.com>
2025-02-15 21:12:34 +0530

    Fix status comparison in GetOrders API to use string '0' and adjust queryset filtering

--- globalStoreApp/orders/orders.py
+++ globalStoreApp/orders/orders.py
@@ -38,10 +38,11 @@ class GetOrders(APIView):
         
         elif isDelivery:
             print("3")
-            if status != 0:
+            if status != '0':
                 querySet=Order.objects.filter(deliveryPartner_id=request.user.id,status=status)
             else:
                 querySet=Order.objects.filter(status=status)
+            querySet=Order.objects.filter(status=status)
             serializer=OrderSerializer(querySet,many=True,context={'request': request})
             for data in serializer.data:
                 data.pop('otp', None)
