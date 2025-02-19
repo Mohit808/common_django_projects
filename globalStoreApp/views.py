f7c08e39aec5e6edb1b926644fe315719417024d
Mohit808 <amityadav.ayx@gmail.com>
2025-02-20 00:04:58 +0530

    .

--- globalStoreApp/views.py
+++ globalStoreApp/views.py
@@ -416,7 +416,7 @@ class AcceptOrders(APIView):
             transactionSerializer=TransactionSerializer(data={"orderId":order_id,"amount":order.totalAmount,"remark":"Delivery success","type":"0","customer":order.store.id}) # order.totalAmount
             if transactionSerializer.is_valid():
                 transactionSerializer.save()
-                wallet, created = Wallet.objects.get_or_create(customer=order.store.id, defaults={'balance': 0} )
+                wallet, created = Wallet.objects.get_or_create(customer_id=order.store.id, defaults={'balance': 0} )
                 Wallet.objects.filter(customer=order.store).update(balance=F('balance')+order.totalAmount)
             else:
                 return customResponse(message= f"{customError(transactionSerializer.errors)}",status=400)
