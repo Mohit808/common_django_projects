449756d153aa5441f32cbac031867cc8b1519f67
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2025-03-04 12:38:00 +0000

    .

--- globalStoreApp/seller/seller.py
+++ globalStoreApp/seller/seller.py
@@ -5,10 +5,11 @@ from rest_framework.views import APIView
 from globalStoreApp.models import *
 from globalStoreApp.my_serializers import *
 from globalStoreApp.custom_response import *
-from django.db.models import Sum
+from django.db.models import Sum, Q
 from datetime import timedelta
 from django.utils import timezone
 from django.db.models.functions import Coalesce
+from django.db.models.functions import TruncDate
 
 
 
@@ -43,13 +44,7 @@ class SellerDashboard(APIView):
         print(f"Orders previous week: {orders_previous_week}")
         print(f"Percentage change: {percentage_change}%")
         
-        #Average order revenue
-        # total_revenue_previous_week = Order.objects.filter(created_at__gte=start_of_previous_week,created_at__lt=end_of_previous_week).aggregate(Sum('discountedTotalAmount'))['discountedTotalAmount__sum']
-        # total_orders_previous_week = Order.objects.filter(created_at__gte=start_of_previous_week,created_at__lt=end_of_previous_week).count()
-        # if total_orders_previous_week > 0:
-        #     average_order_revenue = total_revenue_previous_week / total_orders_previous_week
-        # else:
-        #     average_order_revenue = 0 
+
         seven_days_ago = timezone.now() - timedelta(days=7)
         orders_last_7_days = Order.objects.filter(created_at__gte=seven_days_ago)
         total_revenue_last_7_days = orders_last_7_days.aggregate(total_revenue=Sum(Coalesce('discountedTotalAmount', 'totalAmount')))['total_revenue']
@@ -61,5 +56,25 @@ class SellerDashboard(APIView):
             average_order_revenue_last_7_days = total_revenue_last_7_days / total_orders_last_7_days
         else:
             average_order_revenue_last_7_days = 0
+        
+        
 
-        return customResponse(message="Data fetched successfully", status=200, data={"available_items": available_items, "sold_items": sold_items, "ongoing_orders": ongoing_orders,"total_revenue":total_revenue,"orders_this_week":orders_this_week,"orders_previous_week":orders_previous_week,"percentage_change":percentage_change,"average_order_revenue":average_order_revenue_last_7_days})
+
+        # last 7 day transaction
+        today = timezone.now().date()
+        last_7_days = [today - timedelta(days=i) for i in range(6, -1, -1)]
+        days_of_week = [day.strftime('%a') for day in last_7_days]
+        transactions_last_7_days = Transaction.objects.filter(created_at__gte=last_7_days[-1])
+        total_per_day = (transactions_last_7_days.annotate(date=TruncDate('created_at')) .values('date').annotate(total_credit=Sum('amount', filter=Q(type=0)), total_debit=Sum('amount', filter=Q(type=1)) ).order_by('date'))
+        result = [{day_name: {'credit': 0, 'debit': 0}} for day_name in days_of_week]
+        for entry in total_per_day:
+            day_of_week = entry['date']
+            day_index = last_7_days.index(day_of_week)
+            day_name = days_of_week[day_index]
+            result[day_index] = {
+                day_name: {
+                    'credit': entry['total_credit'] or 0,
+                    'debit': entry['total_debit'] or 0
+                    }
+                }
+        return customResponse(message="Data fetched successfully", status=200, data={"available_items": available_items, "sold_items": sold_items, "ongoing_orders": ongoing_orders,"total_revenue":total_revenue,"orders_this_week":orders_this_week,"orders_previous_week":orders_previous_week,"percentage_change":percentage_change,"average_order_revenue":average_order_revenue_last_7_days,"tran":result})
