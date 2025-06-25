f4f0c026dc88fc797b5e07c3a10b7bbd52268d34
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2025-06-25 08:21:10 +0000

    .

--- dating/views.py
+++ dating/views.py
@@ -6,6 +6,10 @@ from dating.serializers import *
 from rest_framework.pagination import PageNumberPagination
 from django.contrib.auth.models import User
 
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 
 
 def hello(request):
@@ -35,6 +39,10 @@ class DatingRegisterView(APIView):
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 class Login(APIView):
     def post(self,request):
     
