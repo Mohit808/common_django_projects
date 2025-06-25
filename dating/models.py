2cbe5e301a1eb04b604f1309d30d577e4c0dfd7d
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2025-06-25 12:02:19 +0000

    .

--- dating/models.py
+++ dating/models.py
@@ -105,7 +105,7 @@ class UserModel(models.Model):
     location_long=models.CharField(max_length=200)
 
     def __str__(self):
-        return f"{self.name}"+" : "+f"{self.name}"
+        return f"{self.id}"+" : "+f"{self.name}"
 
 class UserPreference(models.Model):
     text=models.TextField()
