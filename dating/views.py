1d7422a0b312ea90adad4d3d2be7d7043615388f
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2025-06-27 12:36:43 +0000

    .

--- dating/views.py
+++ dating/views.py
@@ -132,7 +132,7 @@ class Home(APIView):
         paginated_user_model = paginator.paginate_queryset(user_model, request)
         serialized_users = UserSerializer(paginated_user_model, many=True).data
 
-        likes=LikeDating.objects.filter(receiver=request.user)
+        likes=LikeDating.objects.filter(receiver=request.user.id)
         serialized_like=LikeSerializer(likes, many=True).data
 
         return Response({
