48154a110ba030b9f96bdd214aee5be285b8b0af
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2025-06-30 08:23:47 +0000

    .

--- dating/views.py
+++ dating/views.py
@@ -223,14 +223,11 @@ class AcceptRequest(APIView):
         if not request.data.get('like_id'):
             return customResponse(message="Like ID is required", status=400)
         data = request.data.copy()
-        data['receiver'] = request.user.id
         try:
-            like = LikeDating.objects.get(id=data['like_id'], receiver=data['receiver'])
+            like = LikeDating.objects.get(id=data['like_id'])
         except LikeDating.DoesNotExist:
             return customResponse(message="Like not found", status=404)
 
-        if like.sender == data['receiver']:
-            return customResponse(message="You cannot accept your own like", status=400)
         if Match.objects.filter(sender=like.sender, receiver=like.receiver).exists():
             return customResponse(message="You have already matched with this user", status=400)
         match = Match.objects.create(sender=like.sender, receiver=like.receiver)
