f1326701c747ba554b79c763ee233e369e799177
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2025-07-02 06:08:25 +0000

    .

--- dating/models.py
+++ dating/models.py
@@ -219,6 +219,7 @@ class SponsoredOuting(models.Model):
 
 class Gift(models.Model):
     url= models.URLField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
 
     def __str__(self):
         return f"Gift URL: {self.url}" if self.url else "Gift without URL"
