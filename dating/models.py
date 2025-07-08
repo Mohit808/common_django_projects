232a963ebc477f46a3a14870b077504a5228e901
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2025-07-08 13:33:55 +0000

    .

--- dating/models.py
+++ dating/models.py
@@ -177,8 +177,8 @@ class Subscription(models.Model):
 class DatingNotification(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE, null=True)
     message = models.TextField(null=True)
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False,null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
