715e1b911d7aaac7139546373a799cfd93631158
Mohit808 <35875197+Mohit808@users.noreply.github.com>
2025-07-09 06:01:48 +0000

    .

--- dating/fcm/fcm.py
+++ dating/fcm/fcm.py
@@ -2,12 +2,14 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import messaging
 
-cred = credentials.Certificate("dating/fcm/myngle-firebase-adminsdk-fbsvc-cef355a83f.json")
-firebase_admin.initialize_app(cred)
-
 
 
 def send_push_notification(token, title, body, data=None):
+
+    cred = credentials.Certificate("dating/fcm/myngle-firebase-adminsdk-fbsvc-cef355a83f.json")
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app(cred)
+        
     message = messaging.Message(
         notification=messaging.Notification(
             title=title,
