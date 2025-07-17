153d7b427a6a5919d5e7ef72d518937ce90ad7a0
Amit Yadav <amityadav@theoceann.ai>
2025-07-17 19:26:41 +0530

    .

--- globalStoreApp/my_serializers.py
+++ globalStoreApp/my_serializers.py
@@ -37,6 +37,13 @@ class SellerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Seller
         fields = "__all__"
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        request = self.context.get('request')
+        if request is not None:
+            representation['store_logo'] = request.build_absolute_uri(instance.image.url)
+        return representation
 
 
 
