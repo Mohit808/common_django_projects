1e887301a8c797924c86bc5292b4451924e1fbac
Amit Yadav <amityadav@theoceann.ai>
2025-07-21 11:06:50 +0530

    .

--- common_django_projects/settings.py
+++ common_django_projects/settings.py
@@ -142,6 +142,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10  # Number of items per page
 }
