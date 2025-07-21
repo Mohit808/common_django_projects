14e1e20a83a5dcd6c294a3ebefa15fc5a97d1d8b
Amit Yadav <amityadav@theoceann.ai>
2025-07-21 15:51:38 +0530

    .

--- globalStoreApp/fcm/fcm.py
+++ globalStoreApp/fcm/fcm.py
@@ -66,14 +66,14 @@ class SendNotificationQuick(APIView):
 
     def post(self,request):
         access_token=get_access_token()
-        fcm_token=request.data.get("fcm_token")
+        device_token=request.data.get("device_token")
         headers = {
             "Authorization": f"Bearer {access_token}",
             "Content-Type": "application/json; UTF-8",
         }
         message = {
             "message": {
-                "token": fcm_token,
+                "token": device_token,
                 "notification": {
                     "title": "title",
                     "body": "body"
